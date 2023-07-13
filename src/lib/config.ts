a5704382b1ac60cef8039a4a1dab923bc155f49d
vouneo <vouneo.dev@gmail.com>
2023-07-12 22:34:54 -0400

    Primer commit: Estilos y primeros layouts

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'Vouneo'
+export const description = 'Blog for doomers'
+export const url = dev ? 'http://localhost:5173/' : 'https://vouneo.com/'
